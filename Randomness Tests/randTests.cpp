a5fcde26671e28117b479bbb8bc0cb151bbe11f1
Mel Flygare <mflygare@pdx.edu>
2018-03-03 16:07:56 -0800

    Created frequency test, convert test table to string tested

--- Randomness Tests/randTests.cpp
+++ Randomness Tests/randTests.cpp
@@ -1,5 +1,61 @@
-// randTests implementation
+#include "randTests.h"
+#include <iostream>
+
+int testFrequency(int randList[], int numGroups, int frequencyTable[][NUM_RANGE]){
+    frequencyTable = new int[numGroups][NUM_RANGE];
+    for(int i = 0; i < numGroups; ++i){
+        ++frequencyTable[i / (1000000 / numGroups)][randList[i]];
+    }
+    return 1;
+}
+
+string convertToString(int ** testTable, int numRows){
+    string outputString = NULL;
+
+    for(int i = 0; i < numRows; ++i){
+        for(int j = 0; j < NUM_RANGE; ++j){
+            //outputString.push_back(to_string(testTable[i][j]));
+            outputString.append("herro ");
+        }
+        outputString.push_back('\n');
+    }
+    return outputString;
+}
+
+int writeToFile(string toWrite, string outputFile){
+    ofstream out(outputFile);
+
+    if(out.is_open()){
+        out << toWrite;
+        out.close();
+        return 1;
+    }
+    return 0;
+}
+
+int getRandList(string inputFile, int randList[]){
+    ifstream in(inputFile);
+    char tempNum;
+
+    if(in.is_open()){
+        for(int i = 0; i < 1000000; ++i){
+            in >> tempNum;
+            in.ignore(10, ' ');
+            randList[i] = tempNum - '0';
+        }
+    }
+
+    for(int j = 50; j > 0; --j){
+        cout << randList[1000000 - j] << ' ';
+    }
+    return 1;
+}
+
 
 int main(){
+    int randList[1000000];
+
+    getRandList("mersenneRandList.txt", randList);
+
     return 0;
 }
--- Randomness Tests/randTests.h
+++ Randomness Tests/randTests.h
@@ -1,1 +1,11 @@
-// randTests header
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+const int NUM_RANGE = 10;
+
+int testFrequency(int*, int, int[][NUM_RANGE]);
+string convertToString(int**, int);
+int writeToFile(string, string);
+int getRandList(string, int*);
